7a18f5ee58a34a5560132b2189e062b0c727f04c
Atsushi Watanabe <atsushi.w@ieee.org>
2021-01-30 20:42:08 +0900

    Pass all configs to run

--- run.sh
+++ run.sh
@@ -3,4 +3,8 @@
 docker run -it --rm \
   ${DOCKER_OPTS:-} \
   -v $(pwd)/.runner:/opt/actions-runner/.runner \
+  -v $(pwd)/.credentials:/opt/actions-runner/.credentials \
+  -v $(pwd)/.credentials_rsaparams:/opt/actions-runner/.credentials_rsaparams \
+  -v $(pwd)/.env:/opt/actions-runner/.env \
+  -v $(pwd)/.path:/opt/actions-runner/.path \
   actions-runner ./run.sh $@
