68872c541e4d755b97627c50234201b127abcf47
Atsushi Watanabe <atsushi.w@ieee.org>
2021-01-30 20:38:55 +0900

    Extract credentials

--- config.sh
+++ config.sh
@@ -5,4 +5,8 @@ docker run -it \
   --name actions-runner-config \
   actions-runner ./config.sh $@
 docker cp actions-runner-config:/opt/actions-runner/.runner ./
+docker cp actions-runner-config:/opt/actions-runner/.credentials ./
+docker cp actions-runner-config:/opt/actions-runner/.credentials_rsaparams ./
+docker cp actions-runner-config:/opt/actions-runner/.env ./
+docker cp actions-runner-config:/opt/actions-runner/.path ./
 docker container rm actions-runner-config
