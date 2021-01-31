3a06914538c52d9ef4f78d34074d1d5391b61dd5
Atsushi Watanabe <atsushi.w@ieee.org>
2021-01-31 12:59:49 +0900

    Check file existence (#4)

--- run.sh
+++ run.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+required_files="
+  .runner
+  .credentials
+  .credentials_rsaparams
+  .env
+  .path
+"
+
+opts=
+fail=false
+for file in ${required_files}
+do
+  if [ ! -f ${file} ]
+  then
+    echo "${file} not found" >&2
+    fail=true
+  fi
+  opts="${opts} -v $(pwd)/${file}:/opt/actions-runner/${file}"
+done
+
+if $fail
+then
+  echo $opts
+  exit 1
+fi
+
 exec docker run --rm \
   ${DOCKER_OPTS:-} \
-  -v $(pwd)/.runner:/opt/actions-runner/.runner \
-  -v $(pwd)/.credentials:/opt/actions-runner/.credentials \
-  -v $(pwd)/.credentials_rsaparams:/opt/actions-runner/.credentials_rsaparams \
-  -v $(pwd)/.env:/opt/actions-runner/.env \
-  -v $(pwd)/.path:/opt/actions-runner/.path \
+  ${opts} \
   ghcr.io/at-wat/actions-runner ./run.sh $@
